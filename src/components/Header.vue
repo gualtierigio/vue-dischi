dccdad7d835facfc33671898d255683047779ddd
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-09-29 16:59:16 +0200

    color ok

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <img src="" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+
+header{
+  height: 100px;
+  width: 100%;
+  background-color: rgb(69, 99, 117);
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+main{
+    height: calc(100vh - 100px);
+    background-color: $bg-page-color;
+}
+
+</style>
