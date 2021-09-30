66c23bda873de5a8b21e520c8f47dad625c6f075
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-09-30 16:07:46 +0200

    preparo gli input per la ricerca

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,19 @@
 <template>
     <main class="pt-5">
+        <Search />
         <Album />
     </main>
 </template>
 
 <script>
-import Album from './Album.vue'
+import Album from './Album.vue';
+import Search from './Search.vue';
 
 export default {
     name: 'Main',
     components: {
     Album,
+    Search
   },
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="d-flex">
+        <div>
+            <input class="form-control" type="text" placeholder="chiedi cose">
+        </div>
+        <div>
+            <button class="btn btn-outline-light">Find it</button>
+        </div>
+        <div>
+            <button class="btn btn-outline-dark">Reset</button>
+        </div>
+    </form>
+</template>
+
+<script>
+
+export default {
+    name: 'Search',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+@import '../style/general.scss';
+
+form{
+    min-width: 400px;
+    margin: 50px 50px;
+    justify-content: center;
+}
+
+</style>
